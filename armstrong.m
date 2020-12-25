da43dc78dd03b2450907d032e801305ca034011d
Subhasish Dutta <58630896+SubhasishDutta-Coder@users.noreply.github.com>
2020-12-26 03:30:45 +0600

    Create armstrong.m

---
+++ armstrong.m
@@ -0,0 +1,15 @@
+x=input('Enter number: '); 
+disp(x)
+temp=x; 
+y = 0;
+h=fprintf('%d',x); %% it return number of digits
+   while x > 0
+     t = mod(x,10);
+     y = y+t^h;
+     x = (x-t)/10;
+   end
+if(y==temp)
+    disp( 'Amstrong'); 
+else
+    disp(' not amstrong'); 
+end
